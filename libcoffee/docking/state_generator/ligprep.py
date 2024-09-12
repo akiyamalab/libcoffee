5eb71d92b7cb44f859b3ee926c8e4fb8489b6067
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2024-09-12 01:27:17 +0000

    implementation though it is not tested yet

--- libcoffee/docking/state_generator/ligprep.py
+++ libcoffee/docking/state_generator/ligprep.py
@@ -1,11 +1,43 @@
+import shutil
+import subprocess
+from tempfile import NamedTemporaryFile
+
 from libcoffee.common.path import SDFFile
 from libcoffee.docking.state_generator import StateGeneratorBase
+
+_inputfile_format = """
+INPUT_FILE_NAME   {inputsdf}
+OUT_SD   {outputsdf}
+FORCE_FIELD   16
+EPIK   {enum_ionization_yes_or_no}
+DETERMINE_CHIRALITIES   no
+IGNORE_CHIRALITIES   no
+NUM_STEREOISOMERS   {max_states}
+"""
 
 
 class Ligprep(StateGeneratorBase):
 
-    def run(self) -> "Ligprep":
-        raise NotImplementedError
+    def run(self, file: SDFFile) -> "Ligprep":
+        self.__inputfile = NamedTemporaryFile(suffix=".inp")
+        self.__outputfile = NamedTemporaryFile(suffix=".sdf")
+        with open(self.__inputfile.name, "w") as f:
+            f.write(
+                _inputfile_format.format(
+                    inputsdf=file,
+                    outputsdf=self.__outputfile.name,
+                    enum_ionization_yes_or_no="yes" if self.enum_ionization else "no",
+                    max_states=self.max_states,
+                )
+            )
+            f.flush()
+        n_subtask = self._n_jobs * 10 if self._n_jobs > 1 else 1  # 10 is a magic number
+        subprocess.run(
+            [str(self.exec), self.__inputfile.name, "-NJOBS", n_subtask, "-HOST", f"localhost:{self._n_jobs}", "WAIT"],
+            check=True,
+        )
+        return self
 
     def save(self, path: SDFFile) -> "Ligprep":
-        raise NotImplementedError
+        shutil.copy(self.__outputfile.name, path)
+        return self
