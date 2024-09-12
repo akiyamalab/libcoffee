b0a79cfdf012b3dd87d623f8fb90a5e2265bfea5
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2024-09-12 01:27:36 +0000

    implement Omega though it is not executed

--- libcoffee/docking/conformer_generator/omega.py
+++ libcoffee/docking/conformer_generator/omega.py
@@ -1,11 +1,35 @@
+import shutil
+import subprocess
+import tempfile
+
 from libcoffee.common.path import SDFFile
 from libcoffee.docking.conformer_generator import ConformerGeneratorBase
 
 
 class Omega(ConformerGeneratorBase):
 
-    def run(self) -> "Omega":
-        raise NotImplementedError
+    def run(self, file: SDFFile) -> "Omega":
+        self.__outputfile = tempfile.NamedTemporaryFile(suffix=".sdf")
+        subprocess.run(
+            [
+                str(self.exec),
+                "-mpi_np",
+                self._n_jobs,
+                "-in",
+                file,
+                "-out",
+                self.__outputfile,
+                "-maxConfs",
+                self._max_confs,
+                "-rms",
+                self._min_rmsd,
+                "-eWindoew",
+                self._energy_tolerance,
+            ],
+            check=True,
+        )
+        return self
 
     def save(self, path: SDFFile) -> "Omega":
-        raise NotImplementedError
+        shutil.copy(self.__outputfile.name, path)
+        return self
