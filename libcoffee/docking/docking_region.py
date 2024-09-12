b88d90c9a6b6f089e505eaa822d160d68e5758da
keisuke-yanagisawa <yanagisawa@c.titech.ac.jp>
2024-09-12 15:58:44 +0900

    eboxsize uses heavy atom center, not all atom center

--- libcoffee/docking/docking_region.py
+++ libcoffee/docking/docking_region.py
@@ -12,7 +12,7 @@ class DockingRegion:
 
     @staticmethod
     def from_molecule(mol: MolBase, pitch: tuple[float, float, float] = (1.0, 1.0, 1.0)) -> "DockingRegion":
-        center = mol.get_coordinates().mean(axis=0)
+        center = mol.get_coordinates(only_heavy_atom=True).mean(axis=0)
         size = (_eboxsize(mol),) * 3
         return DockingRegion(center=center, size=size, pitch=pitch)
 
@@ -23,7 +23,7 @@ def _eboxsize(mol: MolBase) -> int:
     https://www.brylinski.org/eboxsize
     """
     _GY_BOX_RATIO: Final[float] = 0.23
-    center = mol.get_coordinates().mean(axis=0)
-    sq_gyration = ((mol.get_coordinates() - center) ** 2).sum(axis=1).mean()
+    center = mol.get_coordinates(only_heavy_atom=True).mean(axis=0)
+    sq_gyration = ((mol.get_coordinates(only_heavy_atom=True) - center) ** 2).sum(axis=1).mean()
     size = sq_gyration**0.5 / _GY_BOX_RATIO
     return int((size + 1) / 2) * 2  # round up to the nearest even number
